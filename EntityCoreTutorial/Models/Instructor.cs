9b60359877716370ca438d7d6594f951c193187f
Nick <kedomir@gmail.com>
2018-01-31 01:59:12 +0000

    Completed part 7 of the EF tutorial.

--- EntityCoreTutorial/Models/Instructor.cs
+++ EntityCoreTutorial/Models/Instructor.cs
@@ -33,7 +33,19 @@ namespace ContosoUniversity.Models
 
         // -----
 
-        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        private ICollection<CourseAssignment> _courseAssignments;
+        public ICollection<CourseAssignment> CourseAssignments
+        {
+            get
+            {
+                return _courseAssignments ?? (_courseAssignments = new List<CourseAssignment>());
+            }
+            set
+            {
+                _courseAssignments = value;
+            }
+        }
+
         public OfficeAssignment OfficeAssignment { get; set; }
     }
 }
